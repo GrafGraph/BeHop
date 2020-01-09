f82e8a39ab16d8e0d2eec2acbb049566218fd89c
Anton Bespalov <anton.bespalov@fh-erfurt.de>
2020-01-09 16:07:53 +0100

    SignUp schöner gemacht

--- views/pages/signUp.php
+++ views/pages/signUp.php
@@ -5,11 +5,11 @@
 <body>
 <h1>Registrierungsformular</h1>
 <form method="POST">
-   <input type="text" name="email" placeholder="E-Mail"><br>
-   <input type="text" name="firstName" placeholder="Vorname"><br>
-   <input type="text" name="lastName" placeholder="Nachname"><br>
-   <input type="password" name="password" placeholder ="Passwort"><br>
-   <input type="password" name="password" placeholder ="Passwort widerholen"><br>
+   <input type="text" name="email" placeholder="E-Mail"><br><br>
+   <input type="text" name="firstName" placeholder="Vorname"><br><br>
+   <input type="text" name="lastName" placeholder="Nachname"><br><br>
+   <input type="password" name="password" placeholder ="Passwort"><br><br>
+   <input type="password" name="password" placeholder ="Passwort widerholen"><br><br>
    <br>
    <button type="submit" name="submit">Registrieren</button>
 </form>
