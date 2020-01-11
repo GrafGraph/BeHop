61c59ef59e370342cb643fceea4430060c09f4fd
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-01-11 17:29:52 +0100

    Erstes Produkt eingefügt!

--- controller/products_controller.php
+++ controller/products_controller.php
@@ -1,10 +1,15 @@
 <?php
 namespace beHop;
 
+
 class ProductsController extends Controller
 {
 	public function actionProducts()
 	{
 		$this->_params['title'] = 'BeHop - Produkte' ;
+
+		// TODO: Aus Url filtern
+		$products = Product::find('id = 1');
+		$this->_params['product0'] = $products[0];
 	}
 }
