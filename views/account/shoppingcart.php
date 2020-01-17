5168d2c9f016a2716df87155478ed3224a91cde7
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-01-17 11:33:24 +0100

    html strings converted: Nice looking now

--- views/account/shoppingcart.php
+++ views/account/shoppingcart.php
@@ -1,49 +1,38 @@
 <h1>Mein Warenkorb</h1><hr>
 <?php 
-$html = '';
 // Empty shoppingCart
-if(empty($shoppingCartItems))
-{
-    $html .=    '<div>
-                    <h2>Dein Warenkorb ist Leer</h2>
-                    <p> Melde dich an um deinen Warenkorb zu sehen und nach Produkten zu suchen!</p>
-                </div>';
-}
-
-else
-{
-    $priceTotal = 0.0;
-
-   foreach($shoppingCartItems as $item)
-   {
-       $priceForPosition = $item['price'] * $item['quantity'];
-       $priceTotal +=  $priceForPosition;
-       $imageUrl = $item['image']['imageUrl'];
-       $imageAltText =$item['image']['altText'];
-
-       $html .= 
-       '<div>
-           <a href="index.php?c=products&a=showProduct&productID='. $item['id'].'" style="max-width:80px;float:left;">
-           <img src="'.$imageUrl.'" alt="'.$imageAltText.'" style="max-height:70px;max-width:70px;"></a>
-           <p>'.$item['name'].', '.$item['color'].'</p>
-           <p>'.$item['price'].'&euro; x '.$item['quantity'].' = '.$priceForPosition.'&euro;</p>
-       </div><br><br><hr>';
-   }
-    
-   $html .= '<br><div><p>Total: '.$priceTotal.'&euro;</p></div>';
-   if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true)
-   {
-       $html .= '<div><form action="index.php?c=account&a=checkout" method="POST">
-       <input type="hidden" name="priceTotal" value='.$priceTotal.'>
-       <input type="submit" name="checkout" value="Proceed to Checkout"></form></div>';
-   }
+if(empty($shoppingCartItems)) : ?>
+    <div>
+        <h2>Dein Warenkorb ist Leer</h2>
+        <p> Melde dich an um deinen Warenkorb zu sehen und nach Produkten zu suchen!</p>
+    </div>
+<? else :
+$priceTotal = 0.0;
+foreach($shoppingCartItems as $item) :
+    $priceForPosition = $item['price'] * $item['quantity'];
+    $priceTotal +=  $priceForPosition;
+    $imageUrl = $item['image']['imageUrl'];
+    $imageAltText =$item['image']['altText'];?>
+    <div>
+        <a href="index.php?c=products&a=showProduct&productID=<?=$item['id']?>" style="max-width:80px;float:left;">
+        <img src=<?=$imageUrl?> alt=<?=$imageAltText?> style="max-height:70px;max-width:70px;"></a>
+        <p><?=$item['name']?>, <?=$item['color']?></p>
+        <p><?=$item['price']?>&euro; x <?=$item['quantity']?> = <?=$priceForPosition?>&euro;</p>
+    </div><br><br><hr>
+<? endforeach;?>
+   <br><div><p>Total= <?=$priceTotal?>&euro;</p></div>
+   <?if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) : ?>
+    <div>
+        <form action="index.php?c=account&a=checkout" method="POST">
+            <input type="hidden" name="priceTotal" value=<?=$priceTotal?>>
+            <input type="submit" name="checkout" value="Proceed to Checkout">
+       </form>
+    </div>
+<? 
     // TODO: Not logged in -> Checkout leads to Login?
     // Checkout button is unavailable until login
-   else
-   {
-       $html .= '<div><p>Melde dich an um zum Checkout zu gelangen</p></div>';
-       $html .= '<div><button title="Log in to be able to check out" disabled>Checkout</button></div>';
-   }
-}
-echo $html;
-?>
+   else : ?>
+        <div><p>Melde dich an um zum Checkout zu gelangen</p></div>
+        <div><button title="Log in to be able to check out" disabled>Checkout</button></div>
+   <? endif;
+endif;?>
--- views/products/products.php
+++ views/products/products.php
@@ -1,39 +1,25 @@
 <h1>Browse our Products</h1>
-<?php 
-$html = '';
-$html .= '<div>
+<div>
     <form method="GET">
     <ul style="list-style-type:none;">
         <!-- hidden fields for controller and action location -->
         <input type="hidden" name="c" value="products">
-        <input type="hidden" name="a" value="products">   
+        <input type="hidden" name="a" value="products">
         <li>
             <label for="productName">NameSearchfield</label>
             <input type="text" name="productName" id="productName" placeholder="Product Name">
         </li>
         <li> 
             <select name="cat">
-                <option value="">--Select a Category--</option>';
-        $html .= getFilterOptions($categories, 'name', 'cat');
-    // foreach($categories as $category)
-    // {
-    // 		$html .= '<option value="'.$category['name'].'"';
-    // 		if(isset($_GET['cat']) && htmlspecialchars($_GET['cat']) == $category)
-    // 		{
-    // 		   $html .=' selected ';
-    // 		}
-    // 		$html .= '>'.$category['name'].'</option>';
-    // }
-
-$html .= '</select>
+                <option value="">--Select a Category--</option>
+                <?=printFilterOptions($categories, 'name', 'cat')?>
+            </select>
         </li>
         <li> 
             <select name="color">
-            <option value="">--Select a Color--</option>';
-        $html .= getFilterOptions($colors,'color','color');
-            // <option value="White">White</option>
-            // <option value="Black">Black</option>
-$html .= '</select>
+                <option value="">--Select a Color--</option>
+                <?=printFilterOptions($colors,'color','color')?>
+            </select>
         </li>
         <!-- <li>
             <label for="maxPrice">Max Price</label>
@@ -41,42 +27,32 @@ $html .= '</select>
         </li> -->
         <li>
             <select name="brand">
-                <option value="">--Select a Brand--</option>';
-        $html .= getFilterOptions($brands, 'brand', 'brand');
-                // <option value="Jordan">Jordan</option>
-                // <option value="BeHop">BeHop</option>
-$html .= '</select>
+                <option value="">--Select a Brand--</option>
+                <?=printFilterOptions($brands, 'brand', 'brand')?>
+            </select>
         </li>
+    <!-- <li>
+            <label for="maxPrice">Max Price</label>
+            <input type="number" id="maxPrice" min="1" max="99999" step="1" name="maxPrice" default>
+        </li> -->
         <li>
             <button type="submit" name="submit">Filter Now</button>
         </li>
     </ul>
     </form>
     <a href="index.php?c=products&a=products"><button>Reset Filters</button></a>
-</div>';
-
-if(!empty($products))
-{
-    foreach($products as $product)
-    {
-        $imageUrl = $product['image']['imageUrl'] ?? '';
-        $imageAltText = $product['image']['altText'] ?? '';
-        $html .= 
-        '<a href="index.php?c=products&a=showProduct&productID='. $product['id'].'">
+</div>
+<? if(!empty($products)) : ?>
+    <? foreach($products as $product) : ?>
+        
+        <a href="index.php?c=products&a=showProduct&productID=<?=product['id']?>">
         <div class="products">
-        <img src="'. $imageUrl .'" alt="'. $imageAltText .'">'.
-            '<div>'.
-            $product['name']. ' only '. $product['price']. '&euro;'.
-            '</div></br>'.
-        '</div></a>';
-    }
-}
-else
-{
-    $html .= '<div><h2>No results found...</h2></div>';
-}
-
-echo $html;
-?>
-
-
+        <img src="<?=$product['image']['imageUrl'] ?? ''?>" alt="<?= $product['image']['altText'] ?? ''?>">
+        <div>
+            <?=$product['name']?> only <?=$product['price'] ?> &euro;
+        </div></br>
+        </div></a>
+    <? endforeach; ?>
+<? else : ?> 
+    <div><h2>No results found...</h2></div>
+<? endif; ?>
