352b0607d7d654a6dd868d273f9765f80cc83704
Maffotter <michael.hopp@fh-erfurt.de>
2020-01-18 17:06:25 +0100

    minor change to unused function

--- core/functions.php
+++ core/functions.php
@@ -1,6 +1,6 @@
 <?php
 
- const DEBUG =true;
+const DEBUG =true;
 // const ERROR =true;
  
 function debug_to_logFile($message, $class = null){
@@ -15,55 +15,15 @@ function getImagesToProductID($productID){
     return Image::find('product_id = ' . $productID);
 }
 
-// Prints selected to display which filter option on products was used
-// function appendSelectedIfSet($html, $filterOption, $category)
+// returns selected to display which filter option on products was used
+// function SelectedIfSet($html, $filterOption, $category)
 // {
+//     $html = '';
 //     if(isset($_GET[$filterOption]) && htmlspecialchars($_GET[$filterOption]) == $category)
 //     {
-//        $html .= ' selected ';
+//         ' selected ';
 //     }
-// }
-
-// appends all filters from the URL to the Where-statement and returns it.
-// if no filters were used, $where will target ALL products.
-// function whereWithFiltersFromUrl()
-// {
-//     $where = '';
-//     if(isset($_GET['cat']) || isset($_GET['productName']) || isset($_GET['color']) || isset($_GET['brand']) || isset($_GET['sale']) || isset($_GET['maxPrice']))
-//     {
-//         if(!empty($_GET['cat']))
-//         {
-//             $category = Category::findOne('name like "%'.htmlspecialchars($_GET['cat']).'%"');
-//             if(!empty($category['id']))
-//             $where .= ' category_id = '.$category['id'].' and';
-//             else
-//             {
-//                 debug_to_logFile('$category["id"]) is empty!');
-//             }
-//         }
-//         if(!empty($_GET['productName']))
-//         {
-//             $where .= ' name like "%'.htmlspecialchars($_GET['productName']).'%" and';
-//         }
-//         if(!empty($_GET['color']))
-//         {
-//             $where .= ' color ="'.htmlspecialchars($_GET['color']).'" and';
-//         }
-//         if(!empty($_GET['brand']))
-//         {
-//             $where .= ' brand like "%'.htmlspecialchars($_GET['brand']).'%" and';
-//         }
-//         if(!empty($_GET['sale']))
-//         {
-//             $where .= ' sales_id is not null and';
-//         }
-//         if(!empty($_GET['maxPrice']))
-//         {
-//             $where .= ' price <= '.htmlspecialchars($_GET['maxPrice']).' and';
-//         }
-//         // substr($where, 0, -4); // remove the last 4 chars-> ' and' // Not needed since we always add ' id is not null'
-//     }
-//     $where .= ' id is not null';
+//     return $html;
 // }
 
 // filterOptions is array of possible options. attribute is needed key for filterOption
