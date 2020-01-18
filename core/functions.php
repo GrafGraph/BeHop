774cb720a22ea188856f466d1bdb5d7b6333b418
Maffotter <michael.hopp@fh-erfurt.de>
2020-01-18 20:51:10 +0100

    Added Logo and Icons for ShoppingCart, Login and Account. Nav Highlights current position in Logo-Red

--- core/functions.php
+++ core/functions.php
@@ -23,6 +23,37 @@ function printSelectedIfSet($filterOption, $option)
     if(isset($_GET[$filterOption]) && $_GET[$filterOption] === $option) : ?>selected <? endif;
 }
 
+// Changes Nav Text-color if site is active
+function highlightNavText($action)
+{
+    $result = '';
+    if(isset($_GET['a']) && htmlspecialchars($_GET['a']) === $action)
+    {
+        $result = '#ff5757';
+    }
+    else
+    {
+        $result = '#ffffff';
+    }
+    return $result;
+}
+
+// Changes Nav color of Icon if site is active
+function highlightNavIcon($action)
+{
+    $result = '';
+    if(isset($_GET['a']) && htmlspecialchars($_GET['a']) === $action)
+    {
+        $result = 'Red';
+    }
+    else
+    {
+        $result = 'White';
+    }
+    return $result;
+}
+
+
 // filterOptions is array of possible options. attribute is needed key for filterOption
 // Example: getFilterOptions($categories, 'name', 'cat');
 function printFilterOptions($filterOptions, $attribute, $urlAttribute)
--- views/pages/index.php
+++ views/pages/index.php
@@ -1,7 +1,7 @@
-<h1>Welcome at BeHop!</h1><br>
+<h1>BeHop</h1>
+<a href="index.php?c=products&a=products&sale=all">
+    <img src="/Git/BeHop/assets/images/sales/SALES.png" alt="Sales">
 
-
-<!-- <div><h2>Galery?</h2></div> -->
 <div><h2>Campaigns/Sales</h2></div>
 <? if(!empty($sales)) : ?>
 <div>
@@ -9,15 +9,15 @@
         <div class="sales">
         <a href="index.php?c=products&a=products&sale=<?=$sale['name']?>">
         <img src="<?=$sale['image']['imageUrl'] ?? ''?>" alt="<?= $sale['image']['altText'] ?? ''?>">
-        </div></a>
+        </a></div>
     <? endforeach; ?>
 </div><br>
 <? endif; ?>
 <div><h2>Highlights</h2></div>
     <div class="hightlights">
-    <a href="index.php?c=products&a=products&cat=shoes">
-    <img src="/Git/BeHop/assets/images/index/sneakersAndMore.jpg" alt="Sneakers and More">
-    </div></a>
+    <a href="index.php?c=products&a=products&cat=Shoes">
+    <img src="/Git/BeHop/assets/images/index/sneakersAndMore.png" alt="Sneakers and More">
+    </a></div>
     <br>
 <div><h2>New Products</h2></div>
 
