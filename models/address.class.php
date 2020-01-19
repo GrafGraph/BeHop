f862cb86a524acae3cd5b3905c773560bff951d7
Maffotter <michael.hopp@fh-erfurt.de>
2020-01-19 11:10:21 +0100

    added method for finding already existing Address in Database

--- models/address.class.php
+++ models/address.class.php
@@ -11,9 +11,21 @@ class Address extends BaseModel
         'city' => ['type' => BaseModel::TYPE_STRING, 'max' => 50], 
         'street' => ['type' => BaseModel::TYPE_STRING, 'max' => 100],
         'number' => ['type' => BaseModel::TYPE_STRING, 'min' => 1, 'max' => 10],
-        'zip' => ['type' => BaseModel::TYPE_STRING, 'min' => 5, 'max' => 5], 
+        'zip' => ['type' => BaseModel::TYPE_STRING, 'min' => 5, 'max' => 5],
+        'country' => ['type' => BaseModel::TYPE_STRING, 'max' => 50]
     ];
 
+    public static function findAddress($addressData)
+    {
+        $where = 'city ="'.$addressData['city'].'" and street ="'
+        .$addressData['street'].'" and number ="'.$addressData['number']
+        .'" and zip ="'.$addressData['zip'].'" and country ="'
+        .$addressData['country'].'"';
+        // foreach($addressData as $key => $attribute)
+        // {
+        // }
+        return self::findOne($where);
+    }
     public static function validateAddress($newAddress, &$insertError){
         $newAddress->validate($insertError);
 
