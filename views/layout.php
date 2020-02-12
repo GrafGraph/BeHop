5054e8ad129dc8cffb8d3054e75d0cc50e5ff2f5
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-02-12 11:36:05 +0100

    Changed logo to round corners

--- views/layout.php
+++ views/layout.php
@@ -1,7 +1,7 @@
 <html lang="de">
 	<head>
 		<title><?=$title?></title>
-		<link rel="shortcut icon" type="image/x-icon" href="assets/images/layout/logoBig.png">
+		<link rel="shortcut icon" type="image/x-icon" href="assets/images/layout/logo.png">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="assets/css/layout.css">
 		<?php if(isset($css) && is_array($css)) : ?>
