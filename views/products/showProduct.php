c9e0a511de0a0238919ceb69f36527e90001fac8
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-02-12 16:54:02 +0100

    Added MediaBreakPoint for Products flexbox

--- views/products/showProduct.php
+++ views/products/showProduct.php
@@ -3,21 +3,21 @@
 <div style="float:left; width:50%;">
     <img src="<?=$images[0]['imageUrl']?>" alt="<?=$images[0]['imageAltText']?>">
 </div>
-<div style="float:right; width:50%;">
-<p><?=$product['name']?></p>
-<p>Color: <?=$product['color']?></p>
-<p>In Stock: <?=$product['numberInStock']?></p>
-<p> <?if(isset($product['discountPrice'])) : ?>
-    <span class="priceOld"> <?=$product['price']?>&euro;</span>
-    <span class="priceNew">  <?=$product['discountPrice']?>&euro;</span>
-    <? else : ?> <?=$product['price']?> &euro;
-    <? endif;?></p>
-<p>Description:</br>
-<q><?=$product['description']?></q></p>
-</br>
-<form method="POST">
-    <label for="quantity">Amount:</label>
-    <input type="number" name="quantity" min="1" max=<?=$product['numberInStock']?> value="1">
-    <button type="submit" name="submit">Add to Cart</button>
-</form>
+<div style="float:right; width:50%;padding:5px;">
+    <p><?=$product['name']?></p>
+    <p>Color: <?=$product['color']?></p>
+    <p>In Stock: <?=$product['numberInStock']?></p>
+    <p> <?if(isset($product['discountPrice'])) : ?>
+        <span class="priceOld"> <?=$product['price']?>&euro;</span>
+        <span class="priceNew">  <?=$product['discountPrice']?>&euro;</span>
+        <? else : ?> <?=$product['price']?> &euro;
+        <? endif;?></p>
+    <p>Description:</br>
+    <q><?=$product['description']?></q></p>
+    </br>
+    <form method="POST">
+        <label for="quantity">Amount:</label>
+        <input type="number" name="quantity" min="1" max=<?=$product['numberInStock']?> value="1">
+        <button type="submit" name="submit">Add to Cart</button>
+    </form>
 </div>
