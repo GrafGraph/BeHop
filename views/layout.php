be3fafaec0475668ce0b81280a4b6ed5f796cf9f
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-02-14 11:07:08 +0100

    Added placeholder for shoppingcartContent in Nav

--- views/layout.php
+++ views/layout.php
@@ -1,12 +1,12 @@
 <html lang="de">
 	<head>
 		<title><?=$title?></title>
-		<link rel="shortcut icon" type="image/x-icon" href="assets/images/layout/logo.png">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="shortcut icon" type="image/x-icon" href="assets/images/layout/logo.png">
 		<link rel="stylesheet" type="text/css" href="assets/css/layout.css">
 		<?php if(isset($css) && is_array($css)) : ?>
 			<?php foreach($css as $index => $file) : ?>
-			<link rel="stylesheet" type="text/css" href="assets/css/<?=$file?>.css">
+				<link rel="stylesheet" type="text/css" href="assets/css/<?=$file?>.css">
 			<?php endforeach; ?>
 		<?php endif; ?>
 	</head>
@@ -14,11 +14,16 @@
 		<div class="content-wrap">
 			<header>
 				<nav class="BeHopGrey">
-					<a href="?a=index" title="Home"><img src="assets/images/layout/logo.png"
-					alt="BeHop-Logo" class="logo"></a>
+					<a href="?a=index" title="Home">
+						<img src="assets/images/layout/logo.png" alt="BeHop-Logo" class="logo">
+					</a>
 					<ul class="navList">
-						<li><a href="index.php?c=products&a=products" title="Products" style="color:<?=highlightNavText('products');?>;">Products</a></li>
-						<li><a href="index.php?a=aboutus" title="About Us" style="color:<?=highlightNavText('aboutus');?>">About Us</a></li>
+						<li>
+							<a href="index.php?c=products&a=products" title="Products" style="color:<?=highlightNavText('products');?>;">Products</a>
+						</li>
+						<li>
+							<a href="index.php?a=aboutus" title="About Us" style="color:<?=highlightNavText('aboutus');?>">About Us</a>
+						</li>
 						<li>
 							<form method="GET" action="index.php?c=products&a=products">
 								<input type="text" name="search" placeholder="Search...">
@@ -26,18 +31,28 @@
 							</form>
 						</li>
 						<?php if(isLoggedIn()) : ?>
-						<li><a href="?c=account&a=logout" title="Logout" style="color:<?=highlightNavText('logout');?>;">Logout</a></li>
-						<li><a href="?c=account&a=account" title="Account">
-						<img src="assets/images/layout/account<?=highlightNavIcon('account');?>.png" alt="Account" class="navIcon">
-						</a></li>
+							<li>
+								<a href="?c=account&a=logout" title="Logout" style="color:<?=highlightNavText('logout');?>;">Logout</a>
+							</li>
+							
+							<li>
+								<a href="?c=account&a=account" title="Account">
+									<img src="assets/images/layout/account<?=highlightNavIcon('account');?>.png" alt="Account" class="navIcon">
+								</a>
+							</li>
+						
 						<?php else : ?>
-						<li><a href="?c=account&a=login" title="Login">
-						<img src="assets/images/layout/account<?=highlightNavIcon('login');?>.png" alt="Login" class="navIcon">
-						</a></li>
+							<li>
+								<a href="?c=account&a=login" title="Login">
+									<img src="assets/images/layout/account<?=highlightNavIcon('login');?>.png" alt="Login" class="navIcon">
+								</a>
+							</li>
 						<?php endif; ?>
 						<li>
-						<a href="index.php?c=account&a=shoppingcart" title="Shopping Cart">
-						<img src="assets/images/layout/shoppingCart<?=highlightNavIcon('shoppingcart');?>.png" alt="Einkaufswagen Bild" class="navIcon" style="margin-right:50px;"></a>
+							<a href="index.php?c=account&a=shoppingcart" title="Shopping Cart">
+								<img src="assets/images/layout/shoppingCart<?=highlightNavIcon('shoppingcart');?>.png" alt="Einkaufswagen Bild" class="navIcon" style="margin-right:50px;">
+								<?=shoppingcartContent();?>
+							</a>
 						</li>
 					</ul>
 				</nav>
