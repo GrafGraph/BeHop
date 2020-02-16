012bd78e1adb61a6965632e38114c2c3f6059142
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-02-16 15:47:12 +0100

    Added Labels to changePassword form

--- views/account/changePassword.php
+++ views/account/changePassword.php
@@ -3,18 +3,19 @@
    <div class="form-wrap">
       <div class="account-form">
          <form method="POST">
-
-         <fieldset>
          <?php if(!empty($errors)) : ?>
             <div class= "error">
                <?php foreach($errors as $error){echo $error."<br>";}?>
             </div>
          <?php endif; ?><br>
+         <label for="password1">Current Password</label>
          <input type="password" name="password1"required placeholder =" Your current password"><br><br>
+         <label for="password2">New Password</label>
          <input type="password" name="password2"required placeholder =" Your new password"><br><br>
+         <label for="password3">Confirm New Password</label>
          <input type="password" name="password3"required placeholder =" Repeat the new password"><br><br>
 
-         <button type="submit" name="submit">change password</button>
+         <button type="submit" name="submit">Change Password</button>
       </div>
       <br>
    </div>
