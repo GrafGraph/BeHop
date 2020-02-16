dfeed3d376ee20ed9345d29734077d5a2b8a449d
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-02-16 16:11:38 +0100

    Changed layout of showProduct and added dark background

--- views/products/showProduct.php
+++ views/products/showProduct.php
@@ -1,31 +1,36 @@
-<div class="product-wrap">   
-    <h1 class="headline">&raquo;<?=$product['name']?>&laquo;</h1>
-    <div class="container-nowrap">
-        <div class="showProduct-item">
-            <img class="productImage" src="<?=$images[0]['imageUrl']?>" alt="<?=$images[0]['imageAltText']?>">
-        </div>
-        <div class="showProduct-item">
-            <p><strong><?=$product['name']?></strong></p>
-            <p> 
-                <?if(isset($product['discountPrice'])) : ?>
-                    <span class="price priceOld"> <?=$product['price']?>&euro;</span>
-                    <span class="price priceNew">  <?=$product['discountPrice']?>&euro;</span>
-                <? else : ?>  
-                    <span class="price"><?=$product['price']?> &euro;</span>
-                <? endif;?>
-            </p>
-            <p><strong>Color:</strong> <?=$product['color']?></p>
-            <p><strong>In Stock:</strong> <?=$product['numberInStock']?></p>
+<h1 class="headline form-background">&raquo;<?=$product['name']?>&laquo;</h1>
+<section class="form-background">
+        <div class="form-wrap">
+            <div class="account-form">
+                <div class="container-wrap">
+                    <div class="showProduct-item">
+                        <img class="productImage" src="<?=$images[0]['imageUrl']?>" alt="<?=$images[0]['imageAltText']?>">
+                    </div>
+                    <div class="showProduct-item">
+                        <p><strong><?=$product['name']?></strong></p>
+                        <p> 
+                            <?if(isset($product['discountPrice'])) : ?>
+                                <span class="price priceOld"> <?=$product['price']?>&euro;</span>
+                                <span class="price priceNew">  <?=$product['discountPrice']?>&euro;</span>
+                            <? else : ?>  
+                                <span class="price"><?=$product['price']?> &euro;</span>
+                            <? endif;?>
+                        </p>
+                        <p><strong>Color:</strong> <?=$product['color']?></p>
+                        <p><strong>In Stock:</strong> <?=$product['numberInStock']?></p>
 
-            <p><strong>Description:</strong></br>
-            <q><?=$product['description']?></q></p>
-            </br>
-            <form method="POST">
-                <label class="inline" for="quantity"><strong>Amount:</strong></label>
-                <input type="number" name="quantity" min="1" max=<?=$product['numberInStock']?> value="1">
-                <br>
-                <button type="submit" name="addToCartSubmit">Add to Cart</button>
-            </form>
+                        <p><strong>Description:</strong></br>
+                        <q><?=$product['description']?></q></p>
+                        </br>
+                        <form method="POST" style="margin:1%; max-width:152px;">
+                            <label style="display:inline;" for="quantity"><strong>Amount:</strong></label>
+                            <input style="width:77px;" type="number" name="quantity" min="1" max=<?=$product['numberInStock']?> value="1">
+                            <br>
+                            <button type="submit" name="addToCartSubmit">Add to Cart</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
+        <br>
+</section>
