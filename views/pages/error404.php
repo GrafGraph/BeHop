10f85bca581448c2eaac05e6378d9d12896ce928
Anton Bespalov <anton.bespalov@fh-erfurt.de>
2020-02-18 12:43:52 +0100

    error 404 page bearbeitet

--- views/pages/error404.php
+++ views/pages/error404.php
@@ -1,4 +1,12 @@
+<br>
 <h1 class="headline">Error 404 - Not Found</h1>
 <section class="center">
-    <p>Page not found!</p>
+    <br>
+    <p>Page not found!</p><br>
 </section>
+<section class="banner">
+<a href="?a=index" title="Home">
+    <img src="assets/images/aboutUs/fashionForThePeople1.png" alt="Fashion for the People">
+</a>
+</section>
+
