df050a914ccd08fa20d404ee83fff991d70f11c9
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-02-19 09:09:36 +0100

    Started Documentation

--- views/pages/projectDocumentation.php
+++ views/pages/projectDocumentation.php
@@ -1,46 +1,137 @@
-<p>Die ausführliche Projektdokumentation ist als strukturierte HMTL-Seite im Impressum verlinkt. Sie enthält auch 
-Informationen zum Aufwand und der Arbeitsteilung im Team. Details finden Sie in den Folien zur Projektaufgabe.</p>
-<h2>Bildquellen</h2>
-<ul>
-    <li>
-        <h3>Canva</h3>
-        <ul>
-            <li>
-                Banner
-            </li>
-            <li>
-                Icons
-            </li>
-            <li>
-                Startseiten-Kachel
-            </li>
-        </ul>
-    </li>
-    <li>
-        <h3>Etre-Fort</h3>
-        <ul>
-            <li>
-                <a href="https://etre-fort.com/shop/stretch-jeans-ef-j2/">Stretch Jeans EF-J2:</a>
-            </li>
-        </ul>
-    </li>
-    <li>
-        <h3>Sidemenclothing</h3>
-        <ul>
-            <li>
-                <a href="https://www.sidemenclothing.com/">Hoodie</a>
-            </li>
-        </ul>
-        https://de.cleanpng.com/png-3wxi02/
-        <br>
-        https://www.nicepng.com/ourpic/u2y3q8y3a9r5y3u2_urban-outdoor-snapback-cap-baseball-cap/
-        <br>
-        http://www.pngmart.com/image/92210
-        <br>
-        https://www.pngfuel.com/free-png/oczup
-        <br>
-        https://www.dmax-shop.de/urban-classics-t-shirt-check-panel-von-urban-classics-7402.html
-        <br>
-        adidas.de
-    </li>
-</ul>
+<link rel="stylesheet" type="text/css" href="assets/css/documentation.css">
+<section class="docu-wrap">
+    <h1>Projektdokumentation</h1>
+    <a href="WEB-WS1920-Projektaufgabe-1.pdf" title="Aufgaben"><h2>Aufgaben</h2></a>
+    <!-- <p>Die ausführliche Projektdokumentation ist als strukturierte HMTL-Seite im Impressum verlinkt. Sie enthält auch 
+    Informationen zum Aufwand und der Arbeitsteilung im Team. Details finden Sie in den Folien zur Projektaufgabe.</p>
+    <p>Außerdem ist ein Impressum (mit Angaben zu Urhebern und Quellen) einzubinden.</p>
+    <p>Auswahl und lokale Einbindung mind. eines Webfonts</p>
+    <p>Auswahlfelder werden durch eine AJAX‐Abfrage vorab mit aktuellen Werten gefüllt.</p>
+    <p>Elemente der Seite (Bilder, Textbereiche) erscheinen erst durch Nutzerinteraktion</p>
+    <p>Mindestens ein Formular soll mehrseitig sein und die Daten sollen per Session weitergereicht werden.</p>
+    <p>Datenbank und Webserver: XAMPP‐Server 7.1 oder neuer (Apache + MySQL/MariaDB</p>
+    <p>Installationshinweise und Zugangsdaten sind in einem gesonderten Text‐ oder PDF‐Dokument zu notieren.</p>
+    <br>
+    <p>
+        Entwicklung und Aufbau der Website sind auf einer weiteren Unterseite, die über das
+        Impressum verlinkt ist, zu dokumentieren. Diese Seite ist sinnvoll zu untergliedern; die
+        grafische Gestaltung kann dagegen schlicht sein.
+        • Inhalt: Beschreibung des fiktiven Kunden und seiner Ziele, sowie der späteren Websitenutzer
+        • Ergebnisse der Recherche zu ähnlichen Seiten (Links, Screenshots)
+        • Allgemeines Seitenlayout (Wireframe) und Verlinkung/Navigationsstruktur (Sitemap)
+        • Design: Begründung zur Wahl des Layouts, der Farben, Schriften,..
+        • Beschreibung der Funktionalitäten für jede Seite (beschriftete Screenshots)
+        • ER‐Modell und relationales Modell der Datenbank (kann als Grafik eingebunden werden)
+        • Rollenmodell
+        • Flussbild für Dateneingabe per mehrseitigem Formular mit Validierung
+        • bewältigte Herausforderungen und Lösungen
+        • Besonderheiten für die Nutzung oder Bewertung
+        – Hinweise auf besondere techn. Anforderungen, bekannte Fehler, unvollständige Implementierungen,
+        "versteckte" Zusatzfeatures...
+        • Projektmanagement‐Dokumentation: Auflistung von Tätigkeiten, zuständiger Person und
+        benötigtem Aufwand
+    </p>
+    <br>
+    <p>
+        Bewertung
+        WS19/20 Webprogrammierung: Projektaufgabe 11
+        Kriterien
+        Anspruchsniveau: Umfang, Komplexität
+        Kenntnisse und Interesse: fundiert und interessiert
+        Vorgehensweise: zielgerichtet und planmäßig
+        Qualität der Ergebnisse: vollständig, richtig und nachvollziehbar
+         "sauberer" Code (HTML, CSS, JS, PHP)
+        • standardkonforme und konsistente Struktur und Gliederung
+        • sinnvolle Aufteilung der Dateien und Ordnerstruktur (nach Vorgabe)
+        • durchgehender Codestyle (nach Vorgabe) und sinnvolle Kommentierung für leichte
+        Nachvollziehbarkeit
+        • sprechende Namen für Dateien, Stile, Variablen, Funktionen etc.
+        • keine überflüssigen Dateien oder Code‐Bestandteile
+        – also gut aufräumen und (auf anderem Rechner) testen: Browserkonsole beobachten, Validator,...!
+         grafische Gestaltung und Bedienbarkeit
+        • klare Gestaltung, passende Farbigkeit
+        • gute Lesbarkeit, verständliche Navigation, gute Bedienbarkeit
+        • Nutzbarkeit auf 2 versch. Bildschirmauflösungen/Fenstergrößen
+        Präsentation + Dokumentation
+    </p>
+    <p>
+        Alle Mitglieder müssen am Ende ein gutes Verständnis von allen Aspekten der
+        Website haben und Fragen dazu beantworten können.
+         Die Arbeitsteilung und der jeweilige Aufwand ist bei der Präsentation kurz und bei
+        der Endabgabe ausführlich zu dokumentieren.
+    </p> -->
+    <h2>Arbeitsteilung</h2>
+    <table>
+        <tr>     
+            <th>Aufgabe</th>
+            <th>Verantwortlicher</th>
+        </tr>
+        <tr>
+            <td>...</td>
+            <td>Michael</td>
+        </tr>
+        <tr>
+            <td>...</td>
+            <td>Anton</td>
+        </tr>
+
+        <tr>
+            <th>Aufwand (Stunden)</th>
+            <th>Entwickler</th>
+        </tr>
+        <tr>
+            <td>200+</td>
+            <td>Michael</td>
+        </tr>
+        <tr>
+            <td>...</td>
+            <td>Anton</td>
+        </tr>
+    </table>
+
+    <h3>Aufwand</h3>
+    <h2>Bildquellen</h2> 
+    <ul>
+        <li>
+            <h3>Canva</h3>
+            <ul>
+                <li>
+                    Banner
+                </li>
+                <li>
+                    Icons
+                </li>
+                <li>
+                    Startseiten-Kachel
+                </li>
+            </ul>
+        </li>
+        <li>
+            <h3>Etre-Fort</h3>
+            <ul>
+                <li>
+                    <a href="https://etre-fort.com/shop/stretch-jeans-ef-j2/">Stretch Jeans EF-J2:</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <h3>Sidemenclothing</h3>
+            <ul>
+                <li>
+                    <a href="https://www.sidemenclothing.com/">Hoodie</a>
+                </li>
+            </ul>
+            https://de.cleanpng.com/png-3wxi02/
+            <br>
+            https://www.nicepng.com/ourpic/u2y3q8y3a9r5y3u2_urban-outdoor-snapback-cap-baseball-cap/
+            <br>
+            http://www.pngmart.com/image/92210
+            <br>
+            https://www.pngfuel.com/free-png/oczup
+            <br>
+            https://www.dmax-shop.de/urban-classics-t-shirt-check-panel-von-urban-classics-7402.html
+            <br>
+            adidas.de
+        </li>
+    </ul>
+</section>
