3fe21be384426eaf5852941aa06d478f13087e22
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-02-19 16:12:46 +0100

    Enhanced Documentation

--- views/pages/projectDocumentation.php
+++ views/pages/projectDocumentation.php
@@ -1,93 +1,95 @@
 <link rel="stylesheet" type="text/css" href="assets/css/documentation.css">
-<section class="docu-wrap">
-    <h1>Projektdokumentation</h1>
-    <a href="WEB-WS1920-Projektaufgabe-1.pdf" title="Aufgaben"><h2>Aufgaben</h2></a>
-    <!-- <p>Die ausführliche Projektdokumentation ist als strukturierte HMTL-Seite im Impressum verlinkt. Sie enthält auch 
-    Informationen zum Aufwand und der Arbeitsteilung im Team. Details finden Sie in den Folien zur Projektaufgabe.</p>
-    <p>Außerdem ist ein Impressum (mit Angaben zu Urhebern und Quellen) einzubinden.</p>
-    <p>Auswahl und lokale Einbindung mind. eines Webfonts</p>
-    <p>Auswahlfelder werden durch eine AJAX‐Abfrage vorab mit aktuellen Werten gefüllt.</p>
-    <p>Elemente der Seite (Bilder, Textbereiche) erscheinen erst durch Nutzerinteraktion</p>
-    <p>Mindestens ein Formular soll mehrseitig sein und die Daten sollen per Session weitergereicht werden.</p>
-    <p>Datenbank und Webserver: XAMPP‐Server 7.1 oder neuer (Apache + MySQL/MariaDB</p>
-    <p>Installationshinweise und Zugangsdaten sind in einem gesonderten Text‐ oder PDF‐Dokument zu notieren.</p>
-    <br>
-    <p>
-        Entwicklung und Aufbau der Website sind auf einer weiteren Unterseite, die über das
-        Impressum verlinkt ist, zu dokumentieren. Diese Seite ist sinnvoll zu untergliedern; die
-        grafische Gestaltung kann dagegen schlicht sein.
-        • Inhalt: Beschreibung des fiktiven Kunden und seiner Ziele, sowie der späteren Websitenutzer
-        • Ergebnisse der Recherche zu ähnlichen Seiten (Links, Screenshots)
-        • Allgemeines Seitenlayout (Wireframe) und Verlinkung/Navigationsstruktur (Sitemap)
-        • Design: Begründung zur Wahl des Layouts, der Farben, Schriften,..
-        • Beschreibung der Funktionalitäten für jede Seite (beschriftete Screenshots)
-        • ER‐Modell und relationales Modell der Datenbank (kann als Grafik eingebunden werden)
-        • Rollenmodell
-        • Flussbild für Dateneingabe per mehrseitigem Formular mit Validierung
-        • bewältigte Herausforderungen und Lösungen
-        • Besonderheiten für die Nutzung oder Bewertung
-        – Hinweise auf besondere techn. Anforderungen, bekannte Fehler, unvollständige Implementierungen,
-        "versteckte" Zusatzfeatures...
-        • Projektmanagement‐Dokumentation: Auflistung von Tätigkeiten, zuständiger Person und
-        benötigtem Aufwand
-    </p>
-    <br>
-    <p>
-        Bewertung
-        WS19/20 Webprogrammierung: Projektaufgabe 11
-        Kriterien
-        Anspruchsniveau: Umfang, Komplexität
-        Kenntnisse und Interesse: fundiert und interessiert
-        Vorgehensweise: zielgerichtet und planmäßig
-        Qualität der Ergebnisse: vollständig, richtig und nachvollziehbar
-         "sauberer" Code (HTML, CSS, JS, PHP)
-        • standardkonforme und konsistente Struktur und Gliederung
-        • sinnvolle Aufteilung der Dateien und Ordnerstruktur (nach Vorgabe)
-        • durchgehender Codestyle (nach Vorgabe) und sinnvolle Kommentierung für leichte
-        Nachvollziehbarkeit
-        • sprechende Namen für Dateien, Stile, Variablen, Funktionen etc.
-        • keine überflüssigen Dateien oder Code‐Bestandteile
-        – also gut aufräumen und (auf anderem Rechner) testen: Browserkonsole beobachten, Validator,...!
-         grafische Gestaltung und Bedienbarkeit
-        • klare Gestaltung, passende Farbigkeit
-        • gute Lesbarkeit, verständliche Navigation, gute Bedienbarkeit
-        • Nutzbarkeit auf 2 versch. Bildschirmauflösungen/Fenstergrößen
-        Präsentation + Dokumentation
-    </p>
-    <p>
-        Alle Mitglieder müssen am Ende ein gutes Verständnis von allen Aspekten der
-        Website haben und Fragen dazu beantworten können.
-         Die Arbeitsteilung und der jeweilige Aufwand ist bei der Präsentation kurz und bei
-        der Endabgabe ausführlich zu dokumentieren.
-    </p> -->
-    <h2>Arbeitsteilung</h2>
-    <table>
-        <tr>     
-            <th>Aufgabe</th>
-            <th>Verantwortlicher</th>
-        </tr>
-        <tr>
-            <td>...</td>
-            <td>Michael</td>
-        </tr>
-        <tr>
-            <td>...</td>
-            <td>Anton</td>
-        </tr>
+<h1 class="headline form-background">Projektdokumentation</h1>
+<section class="center form-background">
+    <section class="docu-wrap">
+        <a href="WEB-WS1920-Projektaufgabe-1.pdf" title="Aufgaben"><h2>Aufgabe</h2></a>
+        <h2>Arbeitsteilung und Gesamtaufwand</h2>
+        <!-- <h3>Gemeinsam</h3>
+        <ul>
+            <li>
+                Gemeinsam
+                <ul>
+                    <li>Produkte einpflegen</li>
+                    <li>Datenbank Konzept</li>
+                    <li>Projektdokumentation</li>
+                    <li>Nav-Desktop<li>
+                    <li>...</li>
+                </ul>
+            </li>
+        </ul> -->
+        <div class="flex-docu">
+            <div class="flex-docu-item">
+                <h3>Michael (200+ Stunden)</h3>
+                <ul>
+                    <li>
+                        Logik und Design
+                        <ul>
+                            <li>Products</li>
+                            <li>showProduct (Detailansicht)</li>
+                            <li>Shopping Cart</li>
+                            <li>Checkout</li>
+                            <li>Login</li>
+                            <li>Account</li>
+                            <li>Nav-Products Dropdown</li>
+                            <li>Legal Details</li>
+                            <li>Landing/Starting Page</li>
+                            <li>Banner, Icons, Startseiten Kachel</li>
+                            <li>...</li>
+                        </ul>
+                    </li>
 
-        <tr>
-            <th>Aufwand (Stunden)</th>
-            <th>Entwickler</th>
-        </tr>
-        <tr>
-            <td>200+</td>
-            <td>Michael</td>
-        </tr>
-        <tr>
-            <td>...</td>
-            <td>Anton</td>
-        </tr>
-    </table>
+                    <li>
+                        Logik
+                        <ul>
+                            <li>Search</li>
+                            <li>Functions</li>
+                            <li>Spezialisierte Basemodel-Methoden</li>
+                            <li>Validierungs-Methoden für Klassen Address und User</li>
+                            <li>...</li>
+                        </ul>
+                    </li>
 
-    <h3>Aufwand</h3>
+                    <li>
+                        Design
+                        <ul>
+                            <li>Changed Password</li>
+                            <li>...</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="flex-docu-item">
+                <h3>Anton (... Stunden)</h3>
+                <ul>
+                    <li>
+                        Logik und Design
+                        <ul>
+                            <li>Sign Up</li>
+                            <li>Nav Tablet- und Mobile-Screen</li>
+                            <li>...</li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        Logik
+                        <ul>
+                            <li>Datenbankanbindung</li>
+                            <li>Basemodel Primitives</li>
+                            <li>Change Password</li>
+                            <li>...</li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        Design
+                        <ul>
+                            <li>Search</li>
+                            <li>...</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <br>
 </section>
