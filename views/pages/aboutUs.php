510fd20ab4dcc4fea8afa361b2b2d892e2f8458e
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-02-25 15:49:54 +0100

    Started about Us

--- views/pages/aboutUs.php
+++ views/pages/aboutUs.php
@@ -1,24 +1,71 @@
-<h1 class="headline">About Us</h1>
-<section class="banner">
-    <img src="assets/images/aboutUs/fashionForThePeople1.png" alt="Fashion for the People">
-</section>
-<article>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing 
-        elit. Aenean commodo ligula eget dolor. Aenean massa 
-        <strong>strong</strong>. Cum sociis natoque penatibus 
-        et magnis dis parturient montes, nascetur ridiculus 
-        mus. Donec quam felis, ultricies nec, pellentesque 
-        eu, pretium quis, sem. Nulla consequat massa quis 
-        enim. Donec pede justo, fringilla vel, aliquet nec, 
-        vulputate eget, arcu. In enim justo, rhoncus ut, 
-        imperdiet a, venenatis vitae, justo. Nullam dictum 
-        felis eu pede <a class="external ext" href="#">link</a> 
-        mollis pretium. Integer tincidunt. Cras dapibus. 
-        Vivamus elementum semper nisi. Aenean vulputate 
-        eleifend tellus. Aenean leo ligula, porttitor eu, 
-        consequat vitae, eleifend ac, enim. Aliquam lorem ante, 
-        dapibus in, viverra quis, feugiat a, tellus. Phasellus 
-        viverra nulla ut metus varius laoreet. Quisque rutrum. 
-        Aenean imperdiet. Etiam ultricies nisi vel augue. 
-        Curabitur ullamcorper ultricies nisi.</p>
-</article>
+<div class="start-wrap">
+    <h1 class="headline">About BeHop</h1>
+    <div style="width:50%;">
+        <img src="assets/images/aboutUs/fashionForThePeople1.png" alt="Fashion for the People">
+    </div>
+    <div class="start-wrap start-container">
+        <div>
+            <h2>Back to the Idea of HipHop</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer free 
+                elit. Aenean commodo ligula eget dolor. Aenean massa 
+                <strong>HipHop</strong>. Cum sociis natoque penatibus 
+                et magnis dis parturient montes, nascetur ridiculus 
+                mus. Donec quam felis, ultricies nec, pellentesque 
+                eu, pretium quis, sem. Nulla consequat massa quis 
+                enim. Donec pede justo, fringilla vel, aliquet nec, 
+                vulputate eget, arcu. In enim justo, rhoncus ut, 
+                imperdiet a, venenatis vitae, justo. Nullam dictum 
+                felis eu pede.
+            </p>
+            <br>    
+        </div>
+        
+        <div class="start-item">
+            <h3>Gender Neutrality as a Brand</h3>
+            <p>
+                Mollis pretium. Integer tincidunt. Cras dapibus. 
+                Vivamus elementum semper nisi. Aenean vulputate 
+                eleifend tellus. Aenean leo ligula, porttitor eu, 
+                consequat vitae, eleifend ac, enim. Aliquam lorem ante, 
+                dapibus in, viverra quis, feugiat a, tellus. Phasellus 
+                viverra nulla ut metus varius laoreet. Quisque rutrum. 
+                Aenean imperdiet. Etiam ultricies nisi vel augue. 
+                Curabitur ullamcorper ultricies nisi.
+            </p>
+            <br>    
+        </div>
+        
+        <div class="start-item">
+            <h3>Focus on the People</h3>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Beatae, quis nisi pariatur mollitia officia magnam itaque
+            rem sunt? Officiis cum laboriosam omnis, harum laborum 
+            doloremque natus. Illum assumenda rerum adipisci!
+            </p>
+            <br>    
+        </div>
+
+        <div class="start-item">
+            <h3>Fashion for Everybody</h3>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Beatae, quis nisi pariatur mollitia officia magnam itaque
+            rem sunt? Officiis cum laboriosam omnis, harum laborum 
+            doloremque natus. Illum assumenda rerum adipisci!
+            </p>
+            <br>    
+        </div>
+        
+        <div>
+            <h2>The Creators</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Dolor consequuntur quibusdam nemo maxime, esse id, quos iure 
+                recusandae ratione accusamus eaque et mollitia cupiditate 
+                voluptas tempora quam adipisci eveniet laudantium?
+            </p>
+            <br>    
+        </div>
+    </article>
+</div>
